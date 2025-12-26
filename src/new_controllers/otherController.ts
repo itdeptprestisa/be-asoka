dfdd37b86f52036329b494215e92633e56bd09bb
wisnuciwun <adiwardhanawisnu@gmail.com>
2025-12-26 21:16:47 +0700

    fix: maintenance

--- src/new_controllers/otherController.ts
+++ src/new_controllers/otherController.ts
@@ -4,30 +4,35 @@ import { Logs } from "../entities/Logs";
 import dataSource from "../config/dataSource";
 import { saveEntity, sendToLavenderFtp } from "../utils";
 import moment from "moment";
+import { handleGojekProof } from "../new_jobs/uploadLocationImage";
 
 export const testFtpUpload = async (
   req: any,
   res: Response,
   next: NextFunction
 ): Promise<void> => {
-  if (process.env.NODE_ENV === "development") {
-    try {
-      const file = req.file;
-      if (!file) {
-        res.json({ success: false, message: "No file uploaded" });
-      }
-      const localPath = file.path; // Multer sets this
-      const remotePath = `/assets/images/customers/sakura-upload-test.png`; // or customize as needed
+  // if (process.env.NODE_ENV === "development") {
+  try {
+    // const file = req.file;
+    // if (!file) {
+    //   res.json({ success: false, message: "No file uploaded" });
+    // }
+    // const localPath = file.path; // Multer sets this
+    // const remotePath = `/assets/images/customers/sakura-upload-test.png`; // or customize as needed
 
-      await sendToLavenderFtp(localPath, remotePath);
-      res.status(200).json({
-        message: "FTP upload successful",
-        remotePath,
-      });
-    } catch (error) {
-      next(error);
-    }
+    const ress = await handleGojekProof(
+      "https://track.gojek.com/?id=47de92a7",
+      101
+    );
+
+    res.status(200).json({
+      message: "FTP upload successful",
+      ress,
+    });
+  } catch (error) {
+    next(error);
   }
+  // }
 };
 
 export const postLog = async (
